b37586858ae688b63c464f1343e8ff81dcc636a6
Richardvillon62 <145690148+Richardvillon62@users.noreply.github.com>
2024-05-30 23:41:00 -0500

    modificacion
    se modifico ususario

--- MODULO USUARIO/FormModificarUsuario.Designer.cs
+++ MODULO USUARIO/FormModificarUsuario.Designer.cs
@@ -76,10 +76,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(249, 19);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(129, 20);
+            this.label1.Size = new System.Drawing.Size(146, 20);
             this.label1.TabIndex = 10;
             this.label1.Text = "Modificar usuario";
             // 
